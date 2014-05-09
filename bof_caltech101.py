a2f0e3923d2781e82488d7f04b1daf4bd25c5af7
namoshi <takio.kurita@gmail.com>
2014-05-09 16:36:21 +0900

    bof_caltech101 temp

---
+++ bof_caltech101.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  2 08:27:25 2014
+
+@author: kurita
+"""
+
+import numpy as np
+
+data = np.loadtxt('caltech101_BoFdata.csv', delimiter=',')
+
+label = np.loadtxt('caltech101_label.csv', delimiter=',')
+
+
+
